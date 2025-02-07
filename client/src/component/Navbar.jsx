8ae32a4f654dd7d2e2435f854bb4ad7c52413a10
Emilie CLARY <emilie.clary@student.junia.com>
2025-02-07 20:17:51 +0100

    [feat] edit navbar title

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import * as api from '../api/AxiosLogin';
 import '../style/Navbar.css';
-// Importer l'image
-//import logo from '/img/logojunia.png';
 
 const Navbar = () => {
   const [login, setLogin] = useState(false);
+  const location = useLocation();
+  const [routeName, setRouteName] = useState('');
 
   useEffect(() => {
     const checkLogin = async () => {
@@ -16,20 +16,38 @@ const Navbar = () => {
     checkLogin();
   }, []);
 
+  useEffect(() => {
+    switch (location.pathname) {
+      case '/':
+        setRouteName('Home');
+        break;
+      case '/pano':
+        setRouteName('Panorama Viewer');
+        break;
+      case '/tour':
+        setRouteName('Tour Viewer');
+        break;
+      case '/admin':
+        setRouteName('Admin Panel');
+        break;
+      default:
+        setRouteName('Menu Principal');
+    }
+  }, [location]);
+
   return (
     <>
       <nav className="relative justify-between bg-white px-2 shadow-lg">
         <div className="flex items-center">
           <img src="/img/logojunia.png" alt="Logo JUNIA" className="h-3/4"></img>
         </div>
-        <div className=" flex items-center text-5xl text-junia-purple font-title"> 
-          Menu Principal 
+        <div className="flex items-center text-5xl text-junia-purple font-title">
+          {routeName}
         </div>
         <div className="flex items-center text-xl text-junia-orange font-title">
           Login Admin
         </div>
       </nav>
-
     </>
   );
 };
